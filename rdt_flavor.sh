fa8f693439cec8223157f5831bb96c642170ad4d
Tony Su <tao.su@intel.com>
2020-02-04 06:50:49 +0000

    add NUMA flavor and XML file
    A new flavor with two NUMA nodes and
    a new VM domain XML file with NUMA nodes

---
+++ rdt_flavor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /opt/stack/devstack/openrc admin demo
+
+flavor_name="rdt.numa"
+openstack flavor delete $flavor_name
+openstack flavor create --vcpus 10 --ram 128 $flavor_name
+openstack flavor set --property hw:cpu_sockets=2 --property hw:cpu_cores=5 --property hw:cpu_threads=1 $flavor_name
+openstack flavor set --property "hw:numa_nodes=2" --property "hw:numa_cpus.0=0-3" --property "hw:numa_cpus.1=4-9" $flavor_name
+openstack flavor set --property "hw:numa_mem.0=64" --property "hw:numa_mem.1=64" $flavor_name
+openstack flavor set --property "hw:cpu_policy=dedicated" $flavor_name
+openstack flavor show $flavor_name
+
